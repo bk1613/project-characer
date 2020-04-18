723da2fe6c3a513e09cbb0bb3b321da5275415e1
Brian Kao <bk1613@nyu.edU>
2020-04-17 21:30:13 -0400

    change test

--- src/test/java/com/revature/test/TestControllerDAO.java
+++ src/test/java/com/revature/test/TestControllerDAO.java
@@ -5,29 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.revature.controllers.AnimeController;
-import com.revature.repository.AnimeCharacterDAO;
-import com.revature.repository.AnimeSeriesDAO;
-import com.revature.repository.CharacterSkillDAO;
+
 
 public class TestControllerDAO {
-	private AnimeController ac = null;
-	private AnimeCharacterDAO cdao = null;
-	private AnimeSeriesDAO sdao = null;
-	private CharacterSkillDAO skdao = null;
-	
+	//private AnimeController ac = null;
+//	private AnimeCharacterDAO cdao = null;
+//	private AnimeSeriesDAO sdao = null;
+//	private CharacterSkillDAO skdao = null;
+//	
 
 	@Before
 	public void setUp() throws Exception {
-		cdao = new AnimeCharacterDAO();
-		sdao = new AnimeSeriesDAO();
-		skdao = new CharacterSkillDAO();
-		ac = new AnimeController(cdao, sdao, skdao);
+//		cdao = new AnimeCharacterDAO();
+//		sdao = new AnimeSeriesDAO();
+//		skdao = new CharacterSkillDAO();
+		//ac = new AnimeController(cdao, sdao, skdao);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testfinAll() {
+//		for(Characters c: cdao.findAll()) {
+//			if(c.getCharId() == 19) {
+//				assertEquals("Kenshiro", c.getName());
+//			}
+//		}
+		
 	}
 
 }
